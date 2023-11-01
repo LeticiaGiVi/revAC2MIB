82d31414ff143b71d303b7d1a75a161aa1249641
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-01 09:50:06 -0300

    revisao

--- OngVida/CadVida.js
+++ OngVida/CadVida.js
@@ -1,78 +1,81 @@
-//requições
-const mongoose = require('mongoose');
-const express = require('express');
-const bodyParser = require('body-parser');
+//requisitando os modulos
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-//configuração do express (server pra pagina e postman)
+//configurando o express para o postman e para usar a pagina
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 const port = 3000;
 
-//configuração do servidor mongodb
-//conecte o mongodb
-mongoose.connect('mongodb://127.0.0.1:27017/cadvida', {
-    useNewUrlParser : true,
-    useUnifiedTopology : true,
-    serverSelectionTimeoutMS : 20000  
-})
+//configurando o banco de dados
+mongoose.connect("mongodb://127.0.0.1:27017/dbvida", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-
-//criando a model solicitada
+//criando a model do seu projeto
 const UsuarioSchema = new mongoose.Schema({
-  
-   nome : {type : String},
-   email : {type :String, required : true },
-   endereco : {type : String},
-   numero : {type : Number},
-   cep : {type : String, required : true},
-   nascimento : {type : Date, required : true}
+  nome: { type: String },
+  email: { type: String, required: true },
+  endereco: { type: String },
+  numero: { type: Number },
+  cep: { type: String, required: true },
+  nascimento: { type: Date, required: true },
 });
 
 const Usuario = mongoose.model("Usuario", UsuarioSchema);
 
-//roteamento padrão
+//configuração dos roteamendos
+//cadastrousuario
+app.post("/cadastrousuario", async (req, res) => {
+  const nome = req.body.nome;
+  const email = req.body.email;
+  const endereco = req.body.endereco;
+  const numero = req.body.numero;
+  const cep = req.body.cep;
+  const nascimento = req.body.nascimento;
 
-app.post("/cadastroOngVida", async(req, res)=>{
-    const nome = req.body.nome;
-    const email = req.body.email;
-    const endereco = req.body.endereco;
-    const numero = req.body.numero;
-    const cep  = req.body.cep;
-    const nascimento = req.body.nascimento
 
-    if(nome == null || email == null || endereco == null || numero == null|| cep == null || nascimento == null){
-        return res.status(400).json({error : "Preenchar todos os campos"});
-    }
+  //validação de campos
+  if(nome == null || email == null || endereco == null || numero == null || cep == null || nascimento == null){
+    return res.status(400).json({error : "Preenchar todos os campos!!!"});
+  }
 
-    const usuario = new Usuario({
-        nome : nome,
-        email : email,
-        endereco : endereco,
-        numero : numero,
-        cep  : cep,
-        nascimento : nascimento
-    })
+  //teste de duplicidade
+  const emailExiste = await Usuario.findOne({email : email});
 
-    try {
-        const newUsuario = await usuario.save();
+  if(emailExiste){
+    return res.status(400).json({error : "O email informado já existe"});
+  }
 
-        res.json({error : null, msg : "Cadastro feito com sucesso", UsuarioId : newUsuario._id});
-    }
-    catch(error){
-        res.status(400).json({error});
-    }
+  
+  const usuario = new Usuario({
+    nome: nome,
+    email: email,
+    endereco: endereco,
+    numero: numero,
+    cep: cep,
+    nascimento: nascimento
+  });
+
+  try {
+    const newUsuario = await usuario.save();
+    res.json({ error: null, msg: "Cadastro ok", UsuarioId: newUsuario._id });
+  } catch (error) {}
 });
 
-app.get("/cadastroOngVida", async(req, res)=>{
-    res.sendFile(__dirname + "/cadastroOngVida.html")
-})
+//rota de get de formulario
+app.get("/cadastrousuario", async (req, res) => {
+  res.sendFile(__dirname + "/cadastrousuario.html");
+});
 
-// app.get("/", async(req, res)=>{
-//     res.sendFile(__dirname + "/index.html")
-// });
+app.get("/", async (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-
-app.listen(port, ()=>{
-    console.log(`servidor rodando na porta ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
